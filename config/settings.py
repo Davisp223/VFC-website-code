2b91ae8069955be5fc6b0baed3b679c1c81c2c30
mcdelaney <mcdelaney@gmail.com>
2020-08-30 12:38:16 -0600

    try to serve static files correctly again

--- config/settings.py
+++ config/settings.py
@@ -13,8 +13,19 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+# BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -32,6 +43,7 @@ ALLOWED_HOSTS = [
     "farmingsimulator.org",
     "www.farmingsimulator.org",
     "localhost",
+    "127.0.0.1",
 ]
 
 
@@ -83,7 +95,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DB_URL = os.getenv("JAWSDB_URL")
 if DB_URL:
-    DEBUG = True
+    DEBUG = False
     import dj_database_url
     DATABASES = {"default": dj_database_url.config(default=DB_URL)}
 else:
@@ -132,11 +144,5 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'vfc.User'
