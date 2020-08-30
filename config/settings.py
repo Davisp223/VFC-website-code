b484a097206ce318f4524ddc709060ad3050d72b
mcdelaney <mcdelaney@gmail.com>
2020-08-30 11:56:51 -0600

    add static root setting for prod

--- config/settings.py
+++ config/settings.py
@@ -126,7 +126,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'vfc.User'
