fa27c03e70b58739656c6031b3f07383b1c46deb
slag223 <48025172+slag223@users.noreply.github.com>
2020-08-31 14:59:35 -0400

    Delete models.py

--- vfc/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class User(AbstractUser):
-    webaccount = models.DecimalField(name="webaccount", default=0.00, 
-                                     decimal_places=2, max_digits=50)
-    points = models.IntegerField(name="points", default=0)
