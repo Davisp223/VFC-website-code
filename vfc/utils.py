fba912a9c34217174a3ab67ae874b1618ea9ed54
slag223 <48025172+slag223@users.noreply.github.com>
2020-08-31 14:59:51 -0400

    Delete utils.py

--- vfc/utils.py
+++
@@ -1,8 +0,0 @@
-
-def parse_request_body(body):
-    params = body.split("&")
-    data = {}
-    for param in params:
-        field, value = param.split('=')
-        data[field] = value
-    return data
