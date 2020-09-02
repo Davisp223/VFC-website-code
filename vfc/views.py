942774b30e10579aa77f6d468c0503e341dd9ba3
slag223 <48025172+slag223@users.noreply.github.com>
2020-09-02 08:52:36 -0400

    Add files via upload

--- vfc/views.py
+++ vfc/views.py
@@ -5,9 +5,9 @@ from django.contrib.auth import authenticate
 from django.contrib import messages
 import random
 from random import randint
-
 from .models import User
 from .utils import parse_request_body
+
 
 logging.basicConfig(level=logging.INFO)
 LOG = logging.getLogger(__name__)
@@ -53,7 +53,7 @@ name_male = (
             "Steven", 
             "Andrew", 
             "Kenneth",
-            "Davis"
+            "Davis",
             )
 name_boy = random.choice(name_male)
 
@@ -61,26 +61,107 @@ pic_male = (
             "https://cdn.discordapp.com/attachments/617827601729191966/750482071553835148/unknown.png",
             "https://cdn.discordapp.com/attachments/617827601729191966/750482133222686780/unknown.png",
             "https://cdn.discordapp.com/attachments/617827601729191966/750482198657761403/unknown.png",
-            "https://cdn.discordapp.com/attachments/617827601729191966/750482234712129636/unknown.png",
-            
+            "https://cdn.discordapp.com/attachments/617827601729191966/750482234712129636/unknown.png",           
             )
 pic_boy = random.choice(pic_male)
-bio_male = (
-            "",
-            "",
-            "",
-            "",
+
+bios_male = (
+            "I am 21 years old and I enjoys walking, tennis and writing. I am creative and loveable, but can also be very standoffish and a bit unintelligent.",
+            "I am 22 years old and love long walks on the beach with you :). I love to create sandcastles then destroy them with 50 pounds of dynamite.",
+            "I am 24 years old and am looking for someone that owns a farm.  I have always wanted to work on a farm.  Looking for a good man",
+            "I am 25 years old and like working on cars.  I am a certified diesel mechanic but only do it for a hobby.",
+            "I am 24 years old and am now trying to start my life with someone who I love.",
+            )
+bio_boy = random.choice(bios_male)
+
+jobs_male = (
+            "Linemen",
+            "Army",
+            "Pilot",
+            "Truck driver",
+            "Mechanic",
+            "Police Officer",
+            "Teacher",
+            "Registered Nurses",
+            "First-Line Supervisors of Retail Sales Workers",
+            "Retail Salespersons",
+            "Software Developers, Applications",
+            "Customer Service Representatives",
+            "Marketing Managers",
+            "First-Line Supervisors of Food Preparation and Serving Workers",
+            "Computer Systems Analysts",
+            "Web Developers",
+            "Management Analysts",
+            "Medical and Health Services Managers",
+            "Information Technology Project Managers",
+            )
+job_boy = random.choice(jobs_male)
+
+name_female = (
+              "Mary", 
+              "Patricia", 
+              "Jennifer", 
+              "Linda", 
+              "Elizabeth", 
+              "Barbara", 
+              "Susan", 
+              "Jessica", 
+              "Sarah", 
+              "Karen", 
+              "Nancy", 
+              "Margaret", 
+              "Lisa", 
+              "Betty", 
+              "Dorothy", 
+              "Sandra", 
+              "Ashley", 
+              "Kimberly", 
+              "Donna", 
+              "Emily",
+              "Clara",
+              )
+name_girl = random.choice(name_female)
+
+pic_female = (
+            "https://cdn.glitch.com/b1c5e0cc-6e5e-47f5-be38-d143eb316e6e%2Fgirl_pic4.png?v=1599008496120",
+            "https://cdn.glitch.com/b1c5e0cc-6e5e-47f5-be38-d143eb316e6e%2Fgirl_pic3.png?v=1599008497357",
+            "https://cdn.glitch.com/b1c5e0cc-6e5e-47f5-be38-d143eb316e6e%2Fgirl_pic2.png?v=1599008498922",
+            "https://cdn.glitch.com/b1c5e0cc-6e5e-47f5-be38-d143eb316e6e%2Fgirl_pic1.png?v=1599008499986",
             
             )
-bio_boy = random.choice(bio_male)
-job_male = (
-            "",
-            "",
-            "",
-            "",
-            
+pic_girl = random.choice(pic_female)
+
+bios_female = (
+            "I am 21 years old and I enjoys walking, tennis and writing. I am creative and loveable, but can also be very standoffish and a bit unintelligent.",
+            "I am 22 years old and love long walks on the beach with you :). I love to create sandcastles then destroy them with 50 pounds of dynamite.",
+            "I am 24 years old and am looking for someone that owns a farm.  I have always wanted to work on a farm.  Looking for a good man",
+            "I am 25 years old and like working on cars.  I am a certified diesel mechanic but only do it for a hobby.",
+            "I am 24 years old and am now trying to start my life with someone who I love.",
             )
-job_boy = random.choice(job_male)
+bio_girl = random.choice(bios_female)
+
+jobs_female = (
+            "Linemen",
+            "Army",
+            "Pilot",
+            "Truck driver",
+            "Mechanic",
+            "Police Officer",
+            "Teacher",
+            "Registered Nurses",
+            "First-Line Supervisors of Retail Sales Workers",
+            "Retail Salespersons",
+            "Software Developers, Applications",
+            "Customer Service Representatives",
+            "Marketing Managers",
+            "First-Line Supervisors of Food Preparation and Serving Workers",
+            "Computer Systems Analysts",
+            "Web Developers",
+            "Management Analysts",
+            "Medical and Health Services Managers",
+            "Information Technology Project Managers",
+            )
+job_girl = random.choice(jobs_female)
 
 
 
@@ -92,7 +173,7 @@ def main(request):
     
     if user is not None:
         template = loader.get_template("main.html")
-        return HttpResponse(template.render({"name": user.username, "money": user.webaccount, "points": user.points, "marriage": user.marriage, "name_boy": name_boy, "pic_boy": pic_boy, "bio_boy": bio_boy, "job_boy": job_boy}))
+        return HttpResponse(template.render({"name": user.username, "money": user.webaccount, "points": user.points, "marriage": user.marriage, "name_boy": name_boy, "pic_boy": pic_boy, "bio_boy": bio_boy, "job_boy": job_boy, "name_girl": name_girl, "pic_girl": pic_girl, "bio_girl": bio_girl, "job_girl": job_girl}))
 
               
             
